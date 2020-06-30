207bb3b9869ae115ba69886cb608cc186f2689c6
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-06-30 20:36:05 +0530

    Driver App Register User

---
+++ app/src/main/java/com/createsapp/uberclonejava/Common.java
@@ -0,0 +1,5 @@
+package com.createsapp.uberclonejava;
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+}
--- app/src/main/java/com/createsapp/uberclonejava/SplashScreenActivity.java
+++ app/src/main/java/com/createsapp/uberclonejava/SplashScreenActivity.java
@@ -2,21 +2,37 @@ package com.createsapp.uberclonejava;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.createsapp.uberclonejava.Model.DriverInfoModel;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
@@ -26,6 +42,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     private List<AuthUI.IdpConfig> providers;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
+
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progress_bar;
+
+    FirebaseDatabase database;
+    DatabaseReference driverInfoRef;
 
     @Override
     protected void onStart() {
@@ -43,11 +66,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash_screen);
         init();
     }
 
     private void init() {
+
+        ButterKnife.bind(this);
+
+        database = FirebaseDatabase.getInstance();
+        driverInfoRef = database.getReference(Common.DRIVER_INFO_REFERENCE);
+
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build());
@@ -55,11 +84,87 @@ public class SplashScreenActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         listener = myFirebaseAuth -> {
             FirebaseUser user = myFirebaseAuth.getCurrentUser();
-            if (user != null)
-                Toast.makeText(this, "Welcom: " + user.getUid(), Toast.LENGTH_SHORT).show();
-            else
+            if (user != null) {
+                checkUserFromFirebase();
+            } else
                 showLoginLayout();
         };
+    }
+
+    private void checkUserFromFirebase() {
+        driverInfoRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            Toast.makeText(SplashScreenActivity.this, "User already register", Toast.LENGTH_SHORT).show();
+                        } else {
+                            showRegisterLayout();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(SplashScreenActivity.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showRegisterLayout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null);
+
+        TextInputEditText edit_first_name = itemView.findViewById(R.id.edt_first_name);
+        TextInputEditText edit_last_name = itemView.findViewById(R.id.edt_last_name);
+        TextInputEditText edit_phone_number = itemView.findViewById(R.id.edit_phone_number);
+
+        Button btn_continue = itemView.findViewById(R.id.btn_register);
+
+        //set data
+        if (FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber() != null &&
+                !TextUtils.isEmpty(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()))
+            edit_phone_number.setText(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+
+        //Set View
+        builder.setView(itemView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(edit_first_name.getText().toString().trim())) {
+                    Toast.makeText(SplashScreenActivity.this, "Please enter first name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(edit_last_name.getText().toString().trim())) {
+                    Toast.makeText(SplashScreenActivity.this, "Please enter last name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(edit_phone_number.getText().toString().trim())) {
+                    Toast.makeText(SplashScreenActivity.this, "Please enter phonenumber", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    DriverInfoModel model = new DriverInfoModel();
+                    model.setFirstName(edit_first_name.getText().toString().trim());
+                    model.setLastName(edit_last_name.getText().toString().trim());
+                    model.setPhoneNumber(edit_phone_number.getText().toString().trim());
+                    model.setRating(0.0);
+
+                    driverInfoRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(model)
+                            .addOnFailureListener(e ->
+                            {
+                                dialog.dismiss();
+                                Toast.makeText(SplashScreenActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            })
+
+                            .addOnSuccessListener(aVoid -> {
+                                Toast.makeText(SplashScreenActivity.this, "Register Succesfully!", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            });
+                }
+
+            }
+        });
     }
 
     private void showLoginLayout() {
@@ -79,6 +184,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void delaySplashScreen() {
+
+        progress_bar.setVisibility(View.VISIBLE);
+
         Completable.timer(3, TimeUnit.SECONDS,
                 AndroidSchedulers.mainThread())
                 .subscribe(() ->
